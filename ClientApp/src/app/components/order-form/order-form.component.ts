6fb58f0e68330662070afa0ae0761d9e29bfcb8f
Nikita Terentjevs <niterissimo@gmail.com>
2024-01-08 06:06:33 +0300

    Reworked form and added basic validation

--- ClientApp/src/app/components/order-form/order-form.component.ts
+++ ClientApp/src/app/components/order-form/order-form.component.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
+import { CALC_CONFIG } from './calc-config'
+import { Order } from './order';
 
 @Component({
   selector: 'app-order-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+      CommonModule,
+      ReactiveFormsModule
+    ],
   templateUrl: './order-form.component.html',
   styleUrl: './order-form.component.scss'
 })
-export class OrderFormComponent {
-  orderFormReady: boolean = false;
+export class OrderFormComponent{
+  orderForm?: any;
   
-  onSubmit(): void {
-    this.orderFormReady = true;
+  eta: any = null;
+  price: any = null;
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  private round(x: number): number {
+    return (Math.round(x*100) / 100);
   }
+
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      amount: ['', Validators.required],
+      pageCount: ['', Validators.required],
+      coverType: ['', Validators.required],
+      bookName: ['', Validators.required],
+      bindingType: ['', Validators.required],
+      format: ['', Validators.required],
+    });
+    console.log(this.orderForm.valid);
+  }
+
+  onSubmit(): void {
+
+    let order: Order = {
+      amount: this.orderForm.get("amount").value,
+      pageCount: this.orderForm.get("pageCount").value,
+      coverType: this.orderForm.get("coverType").value,
+      bookName: this.orderForm.get("bookName").value,
+      bindingType: this.orderForm.get("bindingType").value,
+      format: this.orderForm.get("format").value,
+      sizeX: 240,
+      sizeY: 110
+    }
+
+    this.price = (order.pageCount * CALC_CONFIG.pageCost * order.amount)
+    * CALC_CONFIG.pageFormatMulti[order.format]
+    + CALC_CONFIG.pageBindingPrice[order.bindingType]
+    + CALC_CONFIG.coverTypePrice[order.coverType];
+
+    this.price = this.round(this.price);
+
+    this.eta = ((order.pageCount * CALC_CONFIG.pageTime * order.amount)
+    * CALC_CONFIG.pageFormatMulti[order.format])/(60*60);
+
+    this.eta = this.round(this.eta)
+
+
+
+    //TODO: Send order to Backend
+  }
+
 }
