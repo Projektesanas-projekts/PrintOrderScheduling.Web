8547b694190e7e78748f6eda94a96979e3ef52ae
Nikita Terentjevs <niterissimo@gmail.com>
2024-01-08 06:06:33 +0300

    Reworked form and added basic validation

---
+++ ClientApp/src/app/components/order-form/calc-config.ts
@@ -0,0 +1,20 @@
+export const CALC_CONFIG: any = {
+    pageCost: 0.1,
+    pageTime: 0.01,
+
+    pageFormatMulti: {
+        a4: 2,
+        a5: 1,
+        a6: 0.5
+    },
+
+    pageBindingPrice: {
+        template: 1
+    },
+
+    coverTypePrice: {
+        soft:2,
+        hard:4,
+        newspaper:1,
+    }
+  }
