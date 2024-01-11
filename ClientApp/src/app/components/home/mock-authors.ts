0a67372c0cfe104b184b00c37ae5c632647c4b11
FluendLV <mahito12345@gmail.com>
2024-01-11 22:26:58 +0200

    typos2

--- ClientApp/src/app/components/home/mock-authors.ts
+++ ClientApp/src/app/components/home/mock-authors.ts
@@ -3,9 +3,9 @@ import { Author } from "./author";
 export const AUTHORS: Author[] = [
     {pfp: "./assets/IevaZ.jpg", name: "Ieva", description: "Worked with Katrina to document this project, create presentations, as well as contributed to general research"},
     {pfp: "./assets/Katrina.png", name: "Katrina", description: "Worked with Ieva to document this project, create presentations, as well as contributed to general research"},
-    {pfp: "./assets/Kristians.png", name: "Kristians", description: "Person incharge of Back-End. Most of the back-end issues where adressed by him"},
+    {pfp: "./assets/Kristians.png", name: "Kristians", description: "Person in charge of Back-End. Most of the back-end issues were adressed by him"},
     {pfp: "./assets/NikitaT.png", name: "Nikita", description: "Contributed by making some html pages, styles and some basic angular functionality for those pages"},
-    {pfp: "./assets/OlegsF.png", name: "Olegs", description: "Person incharge of Front-End. All complex tasks and issues where resolved by him"},
+    {pfp: "./assets/OlegsF.png", name: "Olegs", description: "Person in charge of Front-End. All complex tasks and issues were resolved by him"},
     {pfp: "./assets/RihardsL.png", name: "Rihards", description: "Contributed by providing general help with simple tasks and research"},
     {pfp: "./assets/SergejsS.png", name: "Sergejs", description: "Person in charge of math-related stuff when it comes to developing the algorithm for the project as well as general research"}
 ];
